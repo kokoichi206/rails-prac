15cc28d0f8e18a81c3fbef28a2dbc0ddb35c86d1
takahiro.tominaga <tt.300607@gmail.com>
2022-07-10 02:57:39 +0000

    Test full_title testing

--- tutorial/sample_app/test/test_helper.rb
+++ tutorial/sample_app/test/test_helper.rb
@@ -5,6 +5,9 @@ require 'rails/test_help'
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
+  
+  # test 環境でも Application ヘルパーを使えるようにする！
+  include ApplicationHelper
 
   # Add more helper methods to be used by all tests here...
 end
