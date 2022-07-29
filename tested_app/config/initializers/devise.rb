b1b32e6b76bf2862ee3fb4a6a9d4f84babfa8b07
takahiro.tominaga <tt.300607@gmail.com>
2022-07-30 00:37:39 +0900

    change impl

--- tested_app/config/initializers/devise.rb
+++ tested_app/config/initializers/devise.rb
@@ -263,7 +263,9 @@ Devise.setup do |config|
   # should add them to the navigational formats lists.
   #
   # The "*/*" below is required to match Internet Explorer requests.
-  # config.navigational_formats = ['*/*', :html]
+
+  # https://stackoverflow.com/questions/36646226/undefined-method-user-url-for-devise-sessionscontrollercreate
+  config.navigational_formats = ['*/*', :html, :turbo_stream]
 
   # The default HTTP method used to sign out a resource. Default is :delete.
   config.sign_out_via = :delete
